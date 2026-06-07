26e033894c91bbb7c0c12e6cc9731e523c8d7148
talestris <meggycleary@gmail.com>
2026-06-08 00:54:22 +0300

    feat: add webpack config (Mon, Jun 08, 2026 12:54:21 AM)

---
+++ shelter/webpack.config.js
@@ -0,0 +1,67 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { env, argv } = require("process");
+const { type } = require("os");
+
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === "production";
+
+  return {
+    entry: {
+      main: "./src/pages/main/main.js",
+      pets: "./src/pages/pets/pets.js",
+    },
+
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "js/[name].[contenthash:8].js",
+      clean: true,
+    },
+
+    devServer: {
+      static: "./dist",
+      port: 8080,
+      open: true,
+      hot: true,
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            isProduction ? MiniCssExtractPlugin.loader : "style-loader",
+            "css-loader",
+            "sass-loader",
+          ],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif|webp|woff|woff2)$/i,
+          type: "asset/resource",
+          generator: {
+            filename: "assets/[name][ext]",
+          },
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./src/pages/main/index.html",
+        filename: "index.html",
+        chunks: ["main"],
+        inject: "body",
+      }),
+      new HtmlWebpackPlugin({
+        template: "./src/pages/pets/pets.html",
+        filename: "pets.html",
+        chunks: ["pets"],
+        inject: "body",
+      }),
+      new MiniCssExtractPlugin({
+        filename: "css/[name].[contenthash:8].css",
+      }),
+    ],
+    devTool: isProduction ? false : "source-map",
+  };
+};
