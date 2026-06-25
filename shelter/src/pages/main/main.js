dfba7789205d47d9995f46897df2d7686b912cd6
talestris <meggycleary@gmail.com>
2026-06-25 19:55:01 +0300

    feat: add burger functionality (Thu, Jun 25, 2026 07:54:59 PM)

--- shelter/src/pages/main/main.js
+++ shelter/src/pages/main/main.js
@@ -1,2 +1,30 @@
 import "normalize.css";
 import "./main.scss";
+
+const burger = document.querySelector(".burger");
+const nav = document.querySelector(".nav");
+const overlay = document.querySelector(".header__overlay");
+const body = document.body;
+const menuLinks = document.querySelectorAll(".menu__link");
+
+function openMenu() {
+  burger.classList.toggle("open");
+  nav.classList.toggle("open");
+  overlay.classList.toggle("open");
+  body.classList.toggle("noscroll");
+}
+
+function closeMenu() {
+  burger.classList.remove("open");
+  nav.classList.remove("open");
+  overlay.classList.remove("open");
+  body.classList.remove("noscroll");
+}
+
+burger.addEventListener("click", openMenu);
+
+menuLinks.forEach((link) => {
+  link.addEventListener("click", closeMenu);
+});
+
+overlay.addEventListener("click", closeMenu);
